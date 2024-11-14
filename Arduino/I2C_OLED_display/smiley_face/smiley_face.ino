6a9ea6465f402fb2e222b540c0f887b0a992e26f
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-11-14 16:36:16 +0100

    Create smiley_face.ino

---
+++ Arduino/I2C_OLED_display/smiley_face/smiley_face.ino
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+#include "smiley_face.h" 
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+int xx = 20; 
+int yy = 10; 
+
+void setup() {
+    u8g2.begin();
+}
+
+void loop() {
+    smeil_display();
+    delay(500); 
+}
+
+void smeil_display() {
+    const unsigned char* smileImages[] = {
+        semil1, semil2, semil3, semil4, semil5,
+        semil6, semil7, semil8, semil9, semil10,
+        semil11, semil12, semil13, semil14, semil15,
+        semil16, semil17, semil18, semil19, semil20,
+        semil21, semil22, semil23, semil24, semil25,
+        semil26, semil27
+    };
+
+    int delays[] = {
+        40, 50, 40, 40, 40,
+        40, 40, 50, 40, 40,
+        40, 40, 40, 50, 40,
+        40, 50, 40, 40, 50,
+        40, 50, 40, 40, 50,
+        50, 50, 40, 50
+    };
+
+    for (int i = 0; i < sizeof(smileImages) / sizeof(smileImages[0]); i++) {
+        u8g2.firstPage();
+        do {
+            u8g2.drawXBMP(xx, yy, 48, 48, smileImages[i]); 
+        } while (u8g2.nextPage());
+        delay(delays[i]); 
+    }
+}
+}
